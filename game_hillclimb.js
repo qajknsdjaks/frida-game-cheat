cff196c9b5a863a2748aba3328f0417f5de021ff
24414115@qq.com <love1234>
2022-12-21 22:36:39 +0800

    无限 钻石 金币

---
+++ game_hillclimb.js
@@ -0,0 +1,84 @@
+
+function main() {
+        Java.perform(function (){
+        
+
+
+        var moduleName = "libgame.so";
+
+        
+        Interceptor.attach(Module.findExportByName(null, "dlopen"), {
+            onEnter: function(args) {
+                this.lib = Memory.readUtf8String(args[0]);
+                console.log("dlopen called with: " + this.lib);
+            },
+            onLeave: function(retval) {
+
+                if (this.lib.endsWith(moduleName)) {
+                    console.log("ret: " + retval);
+                    var baseAddr = Module.findBaseAddress(moduleName)
+                    console.log("baseAddr1: " + baseAddr)
+
+       
+
+          
+          
+                    }
+
+ 
+            }
+        });
+
+        var fileName
+        var dlopen_ext = Module.findExportByName(null, 'android_dlopen_ext')
+        Interceptor.attach(dlopen_ext, {
+            onEnter: function (args) {
+
+                fileName = ptr(args[0]).readCString()
+                console.log(fileName)
+
+            }, onLeave: function (ret) {
+                if (fileName.endsWith(moduleName)) {
+
+                    var baseAddr = Module.findBaseAddress(moduleName);
+                    console.log("baseAddr2")
+                    console.log(baseAddr)
+
+                    {
+                        setInterval(function(){
+                           
+                           // setTimeScale(5.0)
+                           console.log('11111111')
+                           var monoy = (Memory.readInt(ptr(baseAddr).add(0x684C0C)))
+                           Memory.writeInt(ptr(baseAddr).add(0x684C0C),0xAAAAAAA,)
+                           console.log('monoy',monoy)
+                           var gems = (Memory.readInt(ptr(baseAddr).add(0x684CAC)))
+                           Memory.writeInt(ptr(baseAddr).add(0x684CAC),0xAAAAAAA)
+                           console.log('gems',gems)
+                        }, 10000)
+                        
+                    }
+
+                }
+                return ret
+            }
+        })
+
+
+    })
+}
+setImmediate( main );
+
+
+// frida -U -f com.smokoko.race -l game_hillclimb.js
+
+
+
+
+
+
+
+
+
+
+
---
+++ game_race.js
@@ -0,0 +1,110 @@
+
+function main() {
+        Java.perform(function (){
+
+            
+        if(true)
+        Java.use("android.app.Activity").onCreate .overload('android.os.Bundle').implementation = function (bundle){
+                console.log(this.getClass().getName());
+                var activityName = this.getClass().getName()
+                console.log('')
+                if ( activityName .indexOf('adunit') > 0){
+                    this.onCreate(bundle)
+                    this.finish()
+                } else{
+                    this.onCreate(bundle)
+                }
+
+            
+        }
+        
+
+
+        var moduleName = "libil2cpp.so";
+
+        
+        Interceptor.attach(Module.findExportByName(null, "dlopen"), {
+            onEnter: function(args) {
+                this.lib = Memory.readUtf8String(args[0]);
+                console.log("dlopen called with: " + this.lib);
+            },
+            onLeave: function(retval) {
+
+                if (this.lib != null && this.lib.endsWith(moduleName)) {
+                    console.log("ret: " + retval);
+                    var baseAddr = Module.findBaseAddress(moduleName)
+                    console.log("baseAddr1: " + baseAddr)
+
+
+                    if(false)
+                    {
+                        Interceptor.attach(baseAddr.add(0x10E5190), {
+                            onEnter: function(args) {
+    
+                                Memory.writeInt(ptr(this.context.x20).add(0x10),0xAAAAAAA)
+                                console.log( Memory.readInt(ptr(this.context.x20).add(0x10)))
+                                
+    
+                            }
+                        })
+    
+                        
+                        Interceptor.attach(baseAddr.add(0x10E4FD8), {
+                            onEnter: function(args) {
+                                console.log( Memory.readInt(ptr(this.context.x20).add(0x14)))
+                                Memory.writeInt(ptr(this.context.x20).add(0x14),0xAAAAAAA)
+                                console.log( Memory.readInt(ptr(this.context.x20).add(0x14)))
+                                
+    
+                            }
+                        })
+                    }
+         
+          
+          
+                    }
+
+ 
+            }
+        });
+
+        var fileName
+        var dlopen_ext = Module.findExportByName(null, 'android_dlopen_ext')
+        Interceptor.attach(dlopen_ext, {
+            onEnter: function (args) {
+
+                fileName = ptr(args[0]).readCString()
+                console.log(fileName)
+
+            }, onLeave: function (ret) {
+                if (fileName.endsWith(moduleName)) {
+
+                    var baseAddr = Module.findBaseAddress(moduleName);
+                    console.log("baseAddr2")
+                    console.log(baseAddr)
+
+                }
+                return ret
+            }
+        })
+
+
+    })
+}
+setImmediate( main );
+
+
+
+// frida -U -f com.fingersoft.hillclimb -l game_race.js
+
+
+
+
+
+
+
+
+
+
+
+
